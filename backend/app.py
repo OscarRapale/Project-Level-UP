3c2b415b4ba668af87764d2c0dc75295d7140aa3
OscarRapale <you@example.com>
2024-10-22 17:16:02 -0400

    Integrating websockets for real-time updates of user data in the Frontend

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from src import create_app
+from src import create_app, socketio
 from src.config import get_config
 from src.models import db
 
@@ -8,5 +8,5 @@ with app.app_context():
     db.create_all()  # Ensure the database tables are created
 
 if __name__ == "__main__":
-    # Run the Flask development server
-    app.run(debug=app.config['DEBUG'])
+    # Run the Flask development server with SocketIO support
+    socketio.run(app, debug=app.config['DEBUG'])
