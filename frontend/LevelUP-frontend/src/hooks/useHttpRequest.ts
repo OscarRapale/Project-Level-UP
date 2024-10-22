6de9ea0f6a50be3df6dcc4450d805bb5d31ac434
OscarRapale <you@example.com>
2024-10-22 17:19:58 -0400

    Modifying HTTP request hook so it can make different types of requests

--- frontend/LevelUP-frontend/src/hooks/useHttpRequest.ts
+++ frontend/LevelUP-frontend/src/hooks/useHttpRequest.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 
 interface HttpRequestProps<B> {
   url: string;
-  method?: "GET" | "POST";
+  method?: "GET" | "POST" | "DELETE" | "PUT" | "PATCH";
   body?: B;
 }
 
@@ -23,35 +23,38 @@ const useHttpRequest = <T, B>({
   const [error, setError] = useState<string | null>(null);
   const [loading, setLoading] = useState(false);
 
-  const sendRequest = useCallback(async (config?: Partial<HttpRequestProps<B>>): Promise<T> => {
-    setLoading(true);
-    setError(null);
+  const sendRequest = useCallback(
+    async (config?: Partial<HttpRequestProps<B>>): Promise<T> => {
+      setLoading(true);
+      setError(null);
 
-    try {
-      const token = localStorage.getItem("token");
-      const response = await axios({
-        url: config?.url || url,
-        method: config?.method || method,
-        headers: {
-          "Content-Type": "application/json",
-          ...(token && { Authorization: `Bearer ${token}` }),
-        },
-        data: config?.body || body,
-      });
+      try {
+        const token = localStorage.getItem("token");
+        const response = await axios({
+          url: config?.url || url,
+          method: config?.method || method,
+          headers: {
+            "Content-Type": "application/json",
+            ...(token && { Authorization: `Bearer ${token}` }),
+          },
+          data: config?.body || body,
+        });
 
-      setData(response.data);
-      return response.data;
-    } catch (err) {
-      if (axios.isAxiosError(err)) {
-        setError(err.response?.data.msg || "An error occurred");
-      } else {
-        setError("An unexpected error occurred");
+        setData(response.data);
+        return response.data;
+      } catch (err) {
+        if (axios.isAxiosError(err)) {
+          setError(err.response?.data.msg || "An error occurred");
+        } else {
+          setError("An unexpected error occurred");
+        }
+        throw err;
+      } finally {
+        setLoading(false);
       }
-      throw err;
-    } finally {
-      setLoading(false);
-    }
-  }, [url, method, body]);
+    },
+    [url, method, body]
+  );
 
   return { data, loading, error, sendRequest };
 };
