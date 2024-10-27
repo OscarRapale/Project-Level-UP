29b7ca0394326cfcfec0c31cdf61adedd5375ffb
OscarRapale <you@example.com>
2024-10-27 13:25:47 -0400

    Implementing Dark Mode and Color Mode

--- frontend/LevelUP-frontend/src/App.tsx
+++ frontend/LevelUP-frontend/src/App.tsx
@@ -1,13 +1,15 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./components/Home";
+import Home from "./components/Home/Home";
 import Dashboard from "./components/Dashboard/Dashboard";
 import { UserProvider } from "./context/UserContext";
 import NavBar from "./components/NavBar";
 import Login from "./components/Login/Login";
 import Signup from "./components/Signup/Signup";
-import Leaderboard from "./components/Leaderboard";
-import UserProfile from "./components/UserProfile";
+import Leaderboard from "./components/Leaderboard/Leaderboard";
+import UserProfile from "./components/UserProfile/UserProfile";
+import AboutUsPage from "./components/AboutUsPage/AboutUsPage";
+import Footer from "./components/Footer";
 
 const App: React.FC = () => {
   return (
@@ -19,9 +21,11 @@ const App: React.FC = () => {
           <Route path="/login" element={<Login />} />
           <Route path="/sign-up" element={<Signup />} />
           <Route path="/profile" element={<UserProfile />} />
+          <Route path="/about-us" element={<AboutUsPage />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/leaderboard" element={<Leaderboard />} />
         </Routes>
+        <Footer />
       </Router>
     </UserProvider>
   );
