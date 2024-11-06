d904ce16726227df5ece550600b55aebd3e68841
OscarRapale <you@example.com>
2024-11-05 21:07:43 -0400

    Refactoring code for production

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,13 @@
 from src import create_app, socketio
-from src.config import get_config
-from flask_migrate import Migrate # works better with Docker
 from src.models import db
 
-app = create_app(get_config())
-migrate=Migrate(app,db) # Initialize Flask-Migrate
+app = create_app()
+
+# Ensure the database tables are created
+with app.app_context():
+    db.create_all()
 
 if __name__ == "__main__":
     # Run the Flask development server with SocketIO support
     socketio.run(app, host='0.0.0.0', port=5000)
+
